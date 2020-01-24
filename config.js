15f927a641fee4b0f89f444c214084bc91d4aa2e
taylornok <taylorowenskees@outlook.com>
2020-01-23 20:41:12 -0800

    fixed render issue

---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+    api_key: '059e938e610e6258eec47173f6899268'
+}
--- src/App.js
+++ src/App.js
@@ -13,12 +13,18 @@ class App extends Component {
     };
 
     componentDidMount() {
-        //API WORKING IN BROWSER
-        fetch('https://authenticjobs.com/api/?api_key=&method=aj.jobs.search&keywords=php,mysql&perpage=20')
-            .then(res => res.json())
+        fetch('https://authenticjobs.com/api/?api_key=method=aj.jobs.search&format=json&keywords=php,mysql&perpage=20')
+            .then(res => {
+                // const resJSON = res.json();
+                return res.json();
+            })            
             .then((data) => {
-                console.log(data)
-                this.setState({ listings: data })
+                const results = data.listings.listing
+                this.setState ({listings: results})
+                // console.log(results[0])
+                console.log(this.state.listings)
+                // this.setState({ listings: data })
+                // console.log(this.state);
             })
             .catch(console.log)
     }
